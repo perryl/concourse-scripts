3d23b545e9934d39eb31ba29b78efdaa92216a25
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-07 09:54:23 +0000

    Check which directory I'm in

--- ybd-build.sh
+++ ybd-build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 DIR=$PWD
+pwd
 cd /
 sudo mkdir -p src
+pwd
 cd $DIR
+pwd
 
-sudo ./ybd/ybd.py definitions/system/build-system-x86_64.morph x86_64
+#sudo ./ybd/ybd.py definitions/system/build-system-x86_64.morph x86_64
