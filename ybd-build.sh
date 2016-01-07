b8c82334284032d412dd4eddc020eb5c970c3ad8
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-07 12:49:38 +0000

    Show config.log

--- ybd-build.sh
+++ ybd-build.sh
@@ -9,6 +9,7 @@ echo "BUILDING build-essential\n"
 sudo ./ybd/ybd.py build-essential x86_64
 echo "\n\n\n"
 cat /root/ybd/artifacts/stage1-gcc.842188c5f6d594e1bbaf358262fe8988e7494aec19e84c410b425f87ba2ab0e2.build-log
+cat config.log
 echo "\n\n\n"
 echo "\nBUILDING base-system\n"
 sudo ./ybd/ybd.py definitions/systems/base-system-x86_64-generic.morph x86_64
