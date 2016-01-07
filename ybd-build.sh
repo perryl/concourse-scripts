438ce56e674d7219d1a5e3ba83abacb069fc7b20
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-07 15:15:46 +0000

    Search for config.log

--- ybd-build.sh
+++ ybd-build.sh
@@ -5,12 +5,13 @@ sudo mkdir -p /src
 
 #echo "BUILDING stage1-binutils\n"
 #sudo ./ybd/ybd.py stage1-binutils x86_64
-echo "BUILDING build-essential\n"
+#echo "BUILDING build-essential\n"
 sudo ./ybd/ybd.py build-essential x86_64
-echo "\n\n\n"
-cat /root/ybd/artifacts/stage1-gcc.842188c5f6d594e1bbaf358262fe8988e7494aec19e84c410b425f87ba2ab0e2.build-log
+#echo "\n\n\n"
+#cat /root/ybd/artifacts/stage1-gcc.842188c5f6d594e1bbaf358262fe8988e7494aec19e84c410b425f87ba2ab0e2.build-log
 ls ybd
 ls definitions
+find -name "config.log"
 #echo "\n\n\n"
 #echo "\nBUILDING base-system\n"
 #sudo ./ybd/ybd.py definitions/systems/base-system-x86_64-generic.morph x86_64
