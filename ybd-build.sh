274d19d7f335b899ac4594d6ca04f48c601938ba
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-07 12:12:43 +0000

    Setup to build two systems from buildtests branch

--- ybd-build.sh
+++ ybd-build.sh
@@ -3,5 +3,7 @@
 sudo ./scripts/install-dependency.sh
 sudo mkdir -p /src
 
+echo "BUILDING build-essential\n"
 sudo ./ybd/ybd.py build-essential x86_64
-sudo ./ybd/ybd.py base-system x86_64
+echo "\nBUILDING base-system\n"
+sudo ./ybd/ybd.py definitions/systems/base-system-x86_64-generic.morph x86_64
