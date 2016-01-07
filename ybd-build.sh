b67f9076ca3bd3c7bd30a2d4b7ed51c5b1991d8a
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-07 15:31:56 +0000

    Search for config.log in all directories

--- ybd-build.sh
+++ ybd-build.sh
@@ -11,7 +11,7 @@ sudo ./ybd/ybd.py build-essential x86_64
 #cat /root/ybd/artifacts/stage1-gcc.842188c5f6d594e1bbaf358262fe8988e7494aec19e84c410b425f87ba2ab0e2.build-log
 ls ybd
 ls definitions
-find -name "config.log"
+find / -name "config.log"
 #echo "\n\n\n"
 #echo "\nBUILDING base-system\n"
 #sudo ./ybd/ybd.py definitions/systems/base-system-x86_64-generic.morph x86_64
