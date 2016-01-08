204bd1857694d62ca002e816ccd6464ca89f43fa
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-08 09:02:45 +0000

    Install only pip dependencies in ybd-build

--- ybd-build.sh
+++ ybd-build.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-sudo ./scripts/install-dependency.sh
+pip install pyyaml sandboxlib jsonschema bottle cherrypy requests
+
 sudo mkdir -p /src
 
-#echo "BUILDING stage1-binutils\n"
-#sudo ./ybd/ybd.py stage1-binutils x86_64
-#echo "BUILDING build-essential\n"
 sudo ./ybd/ybd.py build-essential x86_64
-#echo "\n\n\n"
-#cat /root/ybd/artifacts/stage1-gcc.842188c5f6d594e1bbaf358262fe8988e7494aec19e84c410b425f87ba2ab0e2.build-log
-cat /root/ybd/tmp/*/stage1-gcc.build/config.log
-#echo "\n\n\n"
-#echo "\nBUILDING base-system\n"
-#sudo ./ybd/ybd.py definitions/systems/base-system-x86_64-generic.morph x86_64
+sudo ./ybd/ybd.py base-system x86_64
