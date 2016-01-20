67a883ee654c9bceb4d8aaac23794909f72d2ba8
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-20 12:58:11 +0000

    Full system yaml.dump functionality

--- parser.py
+++ parser.py
@@ -43,7 +43,7 @@ class YamlLoadError(Error):
 
 class SystemsParser():
 
-    def open_file(self, morphology, system_name):
+    def open_file(self, morphology):
         '''Takes a file, ensure it is a system file and then open it'''
         with open(morphology, 'r') as f:
             try:
@@ -57,46 +57,41 @@ class SystemsParser():
     def get_strata(self, system_file, morphology):
         ''' Iterates through a yaml stream and finds all the strata'''
         # Iterate through the strata section of the system file
-        morph_path = []
         morph_dir = re.sub('/systems', '', os.path.dirname(morphology))
-        for item in system_file['strata']:
-            morph_path.append('%s/%s' % (morph_dir, item['morph']))
-            return morph_path
+        return ['%s/%s' % (morph_dir, strata['morph']) for strata in system_file['strata']]
 
     def transform_prefix(self, repo):
         return 'baserock' if 'baserock' in repo else 'delta'
 
-    def generate_jobs(self, strata):
+    def get_job_from_strata(self, strata):
         inputs = [{'name': x['name']} for x in strata['chunks']]
         aggregates = [{'get': x['name'], 'resource': x['name'], 'trigger': True} for x in strata['chunks']]
         config = {'inputs': inputs, 'platform': 'linux', 'image': 'docker:///perryl/perryl-concourse#latest', 'run': {'path': './ybd/ybd/py', 'args': ['definitions']}}
         task = {'aggregates': aggregates, 'config': config, 'privileged': True}
         job = {'name': strata['name'], 'public': True, 'plan': [task]}
-        jobs = {'jobs': [job]}
-        resource = [{'name': x['name'], 'type': 'git', 'source': {'uri': 'http://git.baserock.org/git/%s/%s' % (self.transform_prefix(x['repo']), re.search(':(.*)', x['repo']).groups(1)[0]), 'branch': x.get('unpetrify-ref', 'master')}} for x in strata['chunks']]
-        resources = {'resources': resource}
-        system = jobs.copy()
-        system.update(resources)
-        return system
+        return job
+
+    def get_resource_from_chunk(self, x):
+        resource = {'name': x['name'], 'type': 'git', 'source': {'uri': 'http://git.baserock.org/git/%s/%s' % (self.transform_prefix(x['repo']), re.search(':(.*)', x['repo']).groups(1)[0]), 'branch': x.get('unpetrify-ref', 'master')}}
+        return resource
 
     def main(self):
+        strata_yamls = []
         parser = argparse.ArgumentParser(
                  description='Takes Baserock system morphology.')
         parser.add_argument('--system', type=str)
         args = parser.parse_args()
         system_name = os.path.splitext(os.path.basename(args.system))[0]
-        yaml_stream = self.open_file(args.system, system_name)
+        yaml_stream = self.open_file(args.system)
         if yaml_stream['kind'] == 'system':
             # Progress to parsing strata
             strata_paths = self.get_strata(yaml_stream, args.system)
-            for path in strata_paths:
-                strata_stream = self.open_file(path, system_name)
-                system = self.generate_jobs(strata_stream)
-        if yaml_stream['kind'] == 'stratum':
-            # Stratum parsed; parse chunks
-            system = self.generate_jobs(strata_stream)
+            strata_yamls = [self.open_file(x) for x in strata_paths]
+            jobs = [self.get_job_from_strata(x) for x in strata_yamls]
+            resources = [[self.get_resource_from_chunk(x) for x in y['chunks']] for y in strata_yamls]
         else:
             pass
+        system = {'jobs': jobs, 'resources': [x for y in resources for x in y]}
         path = '%s/%s' % (os.getcwd(), system_name)
         if not os.path.isdir(path):
             os.mkdir(path)
