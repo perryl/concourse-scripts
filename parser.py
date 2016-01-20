97c447b2384d1d653ab8db8c5ac55831a6bfe039
Daniel Firth <dan.firth@codethink.co.uk>
2016-01-20 16:31:17 +0000

    Add ybd resource

--- parser.py
+++ parser.py
@@ -67,12 +67,15 @@ class SystemsParser():
     def get_job_from_strata(self, strata, system_name):
         inputs = [{'name': x['name']} for x in strata['chunks']]
         inputs.append({'name': 'definitions'})
+        inputs.append({'name': 'ybd'})
         definitions = {'get': 'definitions', 'resource': 'definitions', 'trigger': True}
+        ybd = {'get': 'ybd', 'resource': 'ybd', 'trigger': True}
         build_depends = [self.open_file('definitions/%s' % x['morph'])['name'] for x in strata.get('build-depends', [])]
         if build_depends:
             definitions.update({'passed': build_depends})
         aggregates = [{'get': x['name'], 'resource': x['name'], 'trigger': True} for x in strata['chunks']]
         aggregates.append(definitions)
+        aggregates.append(ybd)
         config = {'inputs': inputs, 'platform': 'linux', 'image': 'docker:///perryl/perryl-concourse#latest', 'run': {'path': './ybd/ybd/py', 'args': ['definitions/systems/%s.morph' % system_name]}}
         task = {'aggregate': aggregates, 'config': config, 'privileged': True}
         job = {'name': strata['name'], 'public': True, 'plan': [task]}
@@ -101,6 +104,7 @@ class SystemsParser():
             resources_by_strata = [[self.get_resource_from_chunk(x) for x in y['chunks']] for y in strata_yamls]
             resources = [x for y in resources_by_strata for x in y]
             resources.append({'name': 'definitions', 'type': 'git', 'source': {'uri': 'git://git.baserock.org/baserock/baserock/definitions.git', 'branch': 'master'}})
+            resources.append({'name': 'ybd', 'type': 'git', 'source': {'uri': 'http://github.com/locallycompact/ybd', 'branch': 'master'}})
         else:
             pass
         system = {'jobs': jobs, 'resources': resources}
