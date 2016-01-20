6c4c7a4b932181912a1b25296e17fa93d794757d
Lauren Perry <lauren.perry@codethink.co.uk>
2016-01-20 16:02:23 +0000

    Add GPLv2 license to scripts

--- parser.py
+++ parser.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 #
-# parser.py
-# Takes in a given morph system and parses the stratum required. Then parses
-# the stratum and determines all the associated chunks
-# Ultimate goal: output the chunks as YAML resource files for Concourse
-# Copyright (c) Codethink 2016 All Rights Reserved
-
-# plan:
-# 1) open file
-# 2) ensure file is system
-# 3) parse file (using ybd methods?)
-# 4) open strata or error if doesn't exist
-# 5) parse strata and determine chunks
-# For now, let's make this a fairly straightforward functional code and worry
-# about making it class based later
+# Copyright (C) 2016  Codethink Limited
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# =*= License: GPL-2 =*=
 
 
 import argparse
