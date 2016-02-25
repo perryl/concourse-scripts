c2292ff02a372279c8f79869b24b15d0e9a41c62
Mike Williams <mike.williams@codethink.co.uk>
2016-02-25 12:15:24 +0000

    WIP: Some more work on making a strata pipeline

--- strata-generator.py
+++ strata-generator.py
@@ -42,24 +42,71 @@ class YamlLoadError(Error):
 
 class StrataGenerator():
 
+    def create_get_dict(self, resource, additional_keyvals={}):
+        return dict({'get': resource, 'attempts': 2}, **additional_keyvals)
+    
+    def get_ybd_task(self, inputs, ybd_args):
+        sh_args = ['-c', 'echo "kbas-url: $YBD_CACHE_SERVER" >> ybd/ybd.conf; '
+                   'echo "kbas-password: $YBD_CACHE_PASSWORD" >> ybd/ybd.conf; '
+                   'echo "gits: $(pwd)" >> ybd/ybd.conf; '
+                   './ybd/ybd.py %s' % ybd_args]
+        return {'task': 'build', 'privileged': True, 'config':{
+                    'platform': 'linux', 'image': 'docker_image',
+                    'inputs': inputs, 'params': {
+                        'YBD_CACHE_SERVER': '{{ybd-cache-server}}',
+                        'YBD_CACHE_PASSWORD': '{{ybd-cache-password}}'
+                    }, 'run': {'path': 'sh', 'args': sh_args}}}
+
     def main(self):
         '''
         Generate a pipeline for the given strata and tests
         '''
         parser = argparse.ArgumentParser(
             description='Generate a pipeline for a strata and tests')
-        parser.add_argument('--strata', type=str,
+        parser.add_argument('strata', type=str,
                             help='The location of the strata in the \
                                   definitions repo')
-        parser.add_argument('--definitions-url', type=str,
+        parser.add_argument('definitionsurl', type=str,
                             help='The URL of the definitions under \
                                   test')
-        parser.add_argument('--branch', type=str,
+        parser.add_argument('branch', type=str,
                             help='The branch of definitions to be \
                                   tested', default='master')
-        parser.add_argument('--tests', type=str,
+        parser.add_argument('tests', type=str,
                             help='The URL of the tests to be run on \
                                   the built system', default=None)
+        parser.add_argument('test-branch', type=str,
+                            help='The branch of the tests to be run \
+                                  on the built system',
+                            default='master')
         args = parser.parse_args()
 
+        resources = []
+        resources.append({'name': 'definitions', 'type': 'git',
+                          'check_every': '15m', 'source': {
+                              'uri': args.definitionsurl,
+                              'branch': args.branch}})
+        resources.append({'name': 'ybd', 'type': 'git', 'source':
+                          {'uri': 'https://github.com/devcurmudgeon/ybd',
+                           'branch': '16.08'}})
+
+        jobs = []
+        inputs = self.create_get_dict("definitions")
+        inputs.update(self.create_get_dict("ybd"))
+        strata_task = self.get_ybd_task(args.strata, "x86_64")
+        # TODO: check how arch should be defined
         
+        jobs.append({'name': args.strata, 'public': True,
+                     'plan': [{'aggregate': inputs}, strata_task]})
+        print jobs
+        system = {'jobs': jobs, 'resources': resources}
+        with open("file_out.yaml", 'w') as f:
+            stream = yaml.dump(system, default_flow_style=False)
+            f.write(stream.replace("'{{ybd-cache-password}}'",
+                                   "{{ybd-cache-password}}")
+                          .replace("'{{ybd-cache-server}}'",
+                                   "{{ybd-cache-server}}"))
+        print "Done"
+
+if __name__ == "__main__":
+    StrataGenerator().main()
