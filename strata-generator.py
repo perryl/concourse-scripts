1022b65a31114d0eb347b857544ed11e13240047
Mike Williams <mike.williams@codethink.co.uk>
2016-02-25 17:23:43 +0000

    WIP: Further work on strata-generator

--- strata-generator.py
+++ strata-generator.py
@@ -25,6 +25,7 @@ import multiprocessing
 from collections import OrderedDict
 
 
+docker_image = "docker:///benbrown/sandboxlib#latest"
 class Error(Exception):
     pass
 
@@ -51,11 +52,34 @@ class StrataGenerator():
                    'echo "gits: $(pwd)" >> ybd/ybd.conf; '
                    './ybd/ybd.py %s' % ybd_args]
         return {'task': 'build', 'privileged': True, 'config':{
-                    'platform': 'linux', 'image': 'docker_image',
+                    'platform': 'linux', 'image': docker_image,
                     'inputs': inputs, 'params': {
                         'YBD_CACHE_SERVER': '{{ybd-cache-server}}',
                         'YBD_CACHE_PASSWORD': '{{ybd-cache-password}}'
                     }, 'run': {'path': 'sh', 'args': sh_args}}}
+
+    def get_test_task(self):
+        test_aggregates = []
+        test_aggregates.append(self.make_aggregate_dependencies("systems/genivi-demo-platform-x86_64-generic.morph"))
+        
+        sh_args = ['-c', 'echo "Testing is Not Yet Implemented"',
+                   'exit 0']
+        test_task = {'task': 'pretend-to-test', 'config':{
+            'platform': 'linux', 'image': docker_image},
+                     'run': {'path': 'sh', 'args':sh_args}}
+        test_plan = test_aggregates + [test_task]
+        return test_plan
+    
+    def make_aggregate_dependencies(self, dependency):
+        '''
+        Returns a dict of definitions that is required to have passed
+        the given dependency
+        '''
+
+        dependent_aggregates = self.create_get_dict("definitions",
+                                                     {'trigger': True,
+                                                      'passed': dependency})
+        return dependent_aggregates
 
     def main(self):
         '''
@@ -93,20 +117,36 @@ class StrataGenerator():
         jobs = []
         definitions_res = self.create_get_dict("definitions")
         ybd_res = self.create_get_dict("ybd")
-        aggregates = []
-        aggregates.append(definitions_res)
-        aggregates.append(ybd_res)
-        strata_task = self.get_ybd_task(args.strata, "x86_64")
-        strata_plan = aggregates + [strata_task]
-        system_task = self.get_ybd_task("systems/genivi-demo-platform-x86_64-generic.morph", "x86_64")
-        system_plan = aggregates + [system_task]
+
+        # There are three tasks required for the dev workflow: strata,
+        # system and test. Here we define the plans for them
+        strata_aggregates = []
+        strata_aggregates.append(definitions_res)
+        strata_aggregates.append(ybd_res)
+        
+        system_aggregates = []
+        system_aggregates.append(self.make_aggregate_dependencies(args.strata))
+        system_aggregates.append(ybd_res)
+
+        inputs = [{'name': 'definitions'}, {'name': 'ybd'}]
+        strata_task = self.get_ybd_task(inputs, "%s %s" % (args.strata, "x86_64"))
+        strata_plan = strata_aggregates + [strata_task]
+        system_task = self.get_ybd_task(inputs, "systems/genivi-demo-platform-x86_64-generic.morph %s" % ("x86_64"))
+        system_plan = system_aggregates + [system_task]
         # TODO: check how arch and system name should be defined
         
         jobs.append({'name': args.strata, 'public': True,
                      'plan': strata_plan})
         jobs.append({'name': 'gdp-x86_64-generic', 'public': True,
                      'plan': system_plan})
+        if args.tests is not None:
+            test_job = self.get_test_task()
+            jobs.append({'name': 'test', 'public':True,
+                        'plan': test_job})
+        
         system = {'jobs': jobs, 'resources': resources}
+
+
         with open("file_out.yaml", 'w') as f:
             stream = yaml.dump(system, default_flow_style=False)
             f.write(stream.replace("'{{ybd-cache-password}}'",
