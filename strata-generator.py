7d89803eeec9ea3da46a1c0f185482fd921e2850
Lauren Perry <lauren.perry@codethink.co.uk>
2016-02-29 12:20:40 +0000

    PEP8-ify code

--- strata-generator.py
+++ strata-generator.py
@@ -34,43 +34,42 @@ class StrataGenerator():
     def create_get_dict(self, resource, **kwargs):
         # TODO: For a demo, we will be using a vagrant box which does
         # not support attempts. For production, switch this round
-        #return dict({'get': resource, 'attempts': 3}, **kwargs)
+        # return dict({'get': resource, 'attempts': 3}, **kwargs)
         return dict({'get': resource}, **kwargs)
-    
+
     def get_ybd_task(self, inputs, ybd_args):
         sh_args = ['-c', 'echo "kbas-url: $YBD_CACHE_SERVER" >> ybd/ybd.conf; '
-                   'echo "kbas-password: $YBD_CACHE_PASSWORD" >> ybd/ybd.conf; '
-                   'echo "gits: $(pwd)" >> ybd/ybd.conf; '
+                   'echo "kbas-password: $YBD_CACHE_PASSWORD" >> ybd/ybd.conf;'
+                   ' echo "gits: $(pwd)" >> ybd/ybd.conf; '
                    './ybd/ybd.py %s' % ybd_args]
-        return {'task': 'build', 'privileged': True, 'config':{
-                    'platform': 'linux', 'image': docker_image,
-                    'inputs': inputs, 'params': {
-                        'YBD_CACHE_SERVER': '{{ybd-cache-server}}',
-                        'YBD_CACHE_PASSWORD': '{{ybd-cache-password}}'
-                    }, 'run': {'path': 'sh', 'args': sh_args}}}
+        return {'task': 'build', 'privileged': True, 'config': {
+                'platform': 'linux', 'image': docker_image,
+                'inputs': inputs, 'params': {
+                    'YBD_CACHE_SERVER': '{{ybd-cache-server}}',
+                    'YBD_CACHE_PASSWORD': '{{ybd-cache-password}}'},
+                'run': {'path': 'sh', 'args': sh_args}}}
 
     def get_test_task(self):
         test_aggregates = []
         test_aggregates.append(
             self.make_aggregate_dependencies("gdp-x86_64-generic"))
-        
+
         sh_args = ['-c', 'echo "Testing is Not Yet Implemented"',
                    'exit 0']
-        test_task = {'task': 'pretend-to-test', 'config':{
+        test_task = {'task': 'pretend-to-test', 'config': {
             'platform': 'linux', 'image': docker_image},
-                     'run': {'path': 'sh', 'args':sh_args}}
+            'run': {'path': 'sh', 'args': sh_args}}
         test_plan = test_aggregates + [test_task]
         return test_plan
-    
+
     def make_aggregate_dependencies(self, dependency):
         '''
         Returns a dict of definitions that is required to have passed
         the given dependency
         '''
 
-        dependent_aggregates = self.create_get_dict("definitions",
-                                                    **{'trigger': True,
-                                                     'passed': [dependency]})
+        dependent_aggregates = self.create_get_dict("definitions", **{
+            'trigger': True, 'passed': [dependency]})
         return dependent_aggregates
 
     def main(self):
@@ -86,9 +85,11 @@ class StrataGenerator():
         parser.add_argument('--branch', type=str, default='master',
                             help='The branch of definitions to be tested')
         parser.add_argument('--tests', type=str, default=None,
-            help='The URL of the tests to be run on the built system')
+                            help='The URL of the tests to be run on the '
+                            'built system')
         parser.add_argument('--test-branch', type=str, default='master',
-            help='The branch of the tests to be run on the built system')
+                            help='The branch of the tests to be run on the '
+                            'built system')
         args = parser.parse_args()
 
         resources = []
@@ -109,7 +110,7 @@ class StrataGenerator():
         strata_aggregates = []
         strata_aggregates.append(definitions_res)
         strata_aggregates.append(ybd_res)
-        
+
         system_aggregates = []
         system_aggregates.append(self.make_aggregate_dependencies(args.strata))
         system_aggregates.append(ybd_res)
@@ -117,13 +118,14 @@ class StrataGenerator():
         inputs = [{'name': 'definitions'}, {'name': 'ybd'}]
         strata_task = self.get_ybd_task(inputs,
                                         "definitions/strata/%s.morph %s" % (
-                                        args.strata, "x86_64"))
+                                            args.strata, "x86_64"))
         strata_plan = strata_aggregates + [strata_task]
-        system_task = self.get_ybd_task(inputs, "definitions/systems/" \
-            "genivi-demo-platform-x86_64-generic.morph %s" % "x86_64")
+        system_task = self.get_ybd_task(inputs, "definitions/systems/"
+                                        "genivi-demo-platform-x86_64-generic"
+                                        ".morph %s" % "x86_64")
         system_plan = system_aggregates + [system_task]
         # TODO: check how arch and system name should be defined
-        
+
         jobs.append({'name': args.strata, 'public': True,
                      'plan': strata_plan})
         jobs.append({'name': 'gdp-x86_64-generic', 'public': True,
@@ -132,7 +134,7 @@ class StrataGenerator():
             test_job = self.get_test_task()
             jobs.append({'name': 'test', 'public': True,
                         'plan': test_job})
-        
+
         system = {'jobs': jobs, 'resources': resources}
 
         Dumper = yaml.SafeDumper
